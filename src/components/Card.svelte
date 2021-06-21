ed5a5a8102e7861cd70775993c4556d62414e47d
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-06-21 16:40:44 +0200

    expense

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,12 +1,30 @@
 <script>
-  export let expenses;
+  import { getContext } from 'svelte';
+
+  export let id;
+  export let name = '';
+  export let amount = 0;
+
+  const deleteExpense = getContext('delete');
+
+  const setEditedExpense = getContext('edit');
 </script>
 
-<section>
-  <h3>Here your monthly expenses</h3>
-  <ul>
-    {#each expenses as item}
-      <li>{item.name} € {item.amount}</li>
-    {/each}
-  </ul>
-</section>
+<article class="single-expense">
+  <div class="expense-info">
+    <h2>
+      {name}
+    </h2>
+    <h4>amount : €{amount}</h4>
+  </div>
+  <div class="expense-buttons">
+    <button class="expense-btn edit-btn" on:click={() => setEditedExpense(id)}>
+      <i class="fas fa-pen" />
+      EDIT
+    </button>
+    <button class="expense-btn delete-btn" on:click={() => deleteExpense(id)}>
+      <i class="fas fa-trash" />
+      DELETE
+    </button>
+  </div>
+</article>
